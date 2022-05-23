e9fbfdca00e4ecb97bb948935486e8fe3de73823
IsraCardoso <israelalmeidacardoso@gmail.com>
2022-05-23 15:10:29 -0300

    continuing

--- exercicios-web/Javascript.js
+++ exercicios-web/Javascript.js
@@ -836,7 +836,54 @@ objeto.processamento(); // Foco passou a ser o objeto
 // Principios importantes:
 // 1. abstracao
 // 2. encapsulamento
-// 3. herança (prototype)
+// 3. herança (prototype) - tipo 'é um'
 // 4. polimorfismo
+// priorize composição ao invés de herança
 /* -------------------------------------------------------------------------- */
 
+// coleção dinâmica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco = 220
+
+console.log(produto)
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    }, {
+        nome: 'Ana',
+        idade: 42
+    }],
+    calcularValorSeguro: function() {
+        // ...
+    }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro'] = 'Av Gigante'
+console.log(carro)
+
+// delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+console.log(carro)
+console.log(carro.condutores)
+console.log(carro.condutores.length)
+
+/* -------------------------------------------------------------------------- */
