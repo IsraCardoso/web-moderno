a06fb952a27de0ab80becc9cb3e02baa1d47b902
IsraCardoso <israelalmeidacardoso@gmail.com>
2022-06-09 07:28:29 -0300

    continuing

--- exercicios-web/EXERCICIOSREACT/2.calculadora/src/components/button/Button.jsx
+++ exercicios-web/EXERCICIOSREACT/2.calculadora/src/components/button/Button.jsx
@@ -1,8 +1,34 @@
 import React from "react";
-import '././Button.css';
+import "././Button.css";
 
-export default function Button({label}){
-  return(
-    <button className="button">{label}</button>
-  )
-}
+export default props => 
+<button className={`button ${props.operation? 'operation':''}
+${props.double? 'double':''}
+${props.triple? 'triple':''}`}>{props.label}<button/>
+
+
+
+
+
+
+{/* export default function Button(props) {
+	let classes = "button";
+	classes += props.operation ? " operation" : "";
+	classes += props.double ? " double" : "";
+	classes += props.triple ? " triple" : "";
+
+	return (
+				<button
+		      onClick={e => props.click && props.click(props.label)} funcionamento?
+					className={
+						classes
+						  `button
+						${props.operation ? 'operation' : ''}
+						${props.double ? 'double' : ''}
+						${props.triple ? 'triple' : ''}`
+					}
+				>
+					{props.label}
+				</button>
+			);
+		} */}
--- exercicios-web/EXERCICIOSREACT/2.calculadora/src/main/Calculator.jsx
+++ exercicios-web/EXERCICIOSREACT/2.calculadora/src/main/Calculator.jsx
@@ -1,31 +1,63 @@
 import React from "react";
 import "./Calculator.css";
 import Button from "../components/button/Button";
+import Dispaly from "../components/display/Display";
+import Display from "../components/display/Display";
 
 export default class Calculator extends React.Component {
 
-  render(){
-    return (
-      <div className="calculator">
-        <Button label={'AC'}/>
-        <Button label={'/'}/>
-        <Button label={'7'}/>
-        <Button label={'8'}/>
-        <Button label={'9'}/>
-        <Button label={'*'}/>
-        <Button label={'4'}/>
-        <Button label={'5'}/>
-        <Button label={'6'}/>
-        <Button label={'-'}/>
-        <Button label={'1'}/>
-        <Button label={'2'}/>
-        <Button label={'3'}/>
-        <Button label={'+'}/>
-        <Button label={'0'}/>
-        <Button label={'.'}/>
-        <Button label={'='}/>
-      </div>
-    );
-  }
 
+	clearMemory() {
+		/////////////////////////funções dentro e fora do render??
+		console.log("limpar memoria");
+	}
+	setOperation(operation) {
+		console.log(operation);
+	}
+	addDigit(n) {
+		console.long(n);
+	}
+
+	render() {
+		const addDigit = (n) => this.addDigit(n);
+		const setOperation = (n) => this.setOperation(n);
+		const clearMemory = () => this.clearMemory();
+
+		return (
+			<div className="calculator">
+				<Display value={100} />
+				<Button
+					label={"AC"}
+					triple
+					operation
+					click={() => this.clearMemory()}
+				/>
+				<Button label={"/"} operation click={(e) => this.setOperation(e)} />
+				<Button label={"7"} click={(e) => this.addDigit(e)} />
+				<Button label={"8"} click={(e) => this.addDigit(e)} />
+				<Button label={"9"} click={(e) => this.addDigit(e)} />
+				<Button label={"*"} operation click={(e) => this.setOperation(e)} />
+				<Button label={"4"} click={(e) => this.addDigit(e)} />
+				<Button label={"5"} click={(e) => this.addDigit(e)} />
+				<Button label={"6"} click={(e) => this.addDigit(e)} />
+				<Button label={"-"} operation click={(e) => this.setOperation(e)} />
+				<Button label={"1"} click={(e) => this.addDigit(e)} />
+				<Button label={"2"} click={(e) => this.addDigit(e)} />
+				<Button label={"3"} click={(e) => this.addDigit(e)} />
+				<Button label={"+"} operation click={(e) => this.setOperation(e)} />
+				<Button label={"0"} click={(e) => this.addDigit(e)} />
+				<Button label={"."} operation click={(e) => this.setOperation(e)} />
+				<Button label={"="} operation click={(e) => this.setOperation(e)} />
+			</div>
+		);
+	}
 }
+
+  // 	// assim ficaria sem as consts em render e as chamadas são diretas
+	// // sem arrowfunctions e '(e)' nos buttons
+	// constructor(props) {
+	// 	super(props);
+	// 	this.clearMemory = this.clearMemory.bind(this);
+	// 	this.setOperation = this.setOperation.bind(this);
+	// 	this.addDigit = this.addDigit.bind(this);
+	// }
